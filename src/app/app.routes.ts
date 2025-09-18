6af87916d57244c5c1d96a04fc77b625c4984437
jyothi <jyothi@gmail.com>
2025-09-17 21:10:14 -0500

    Added a test component with routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MyTestComponentComponent } from './my-test-component/my-test-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: '',
+    component: AppComponent,
+},
+{
+    path: 'test',
+    component: MyTestComponentComponent,
+}];
---
+++ src/app/my-test-component/my-test-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-test-component',
+  imports: [],
+  templateUrl: './my-test-component.component.html',
+  styleUrl: './my-test-component.component.css'
+})
+export class MyTestComponentComponent {
+
+}
